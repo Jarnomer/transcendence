66f29d737b53582b32f6efa88c7821ddcfb7fd51
Your Name <you@example.com>
2025-02-15 14:36:14 +0200

    Changed flake.nix to use flake-utils

--- flake.nix
+++ flake.nix
@@ -3,68 +3,53 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      flake-utils,
     }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
-      forAllSystems =
-        f:
-        builtins.listToAttrs (
-          map (system: {
-            name = system;
-            value = f system;
-          }) supportedSystems
-        );
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          nodePkgs = [
-            "typescript"
-            "vite"
-            "tailwindcss"
-            "babylonjs"
-            "sqlite3"
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        nodePkgs = [
+          "typescript"
+          "vite"
+          "tailwindcss"
+          "babylonjs"
+          "sqlite3"
+        ];
+        installNodePkgs = ''
+          echo "Installing dependencies: ${builtins.concatStringsSep " " nodePkgs}..."
+          npm install --save-dev ${builtins.concatStringsSep " " nodePkgs}
+        '';
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_23
+            sqlite
+            fish
           ];
-          installNodePkgs = ''
-            echo "Installing dependencies: ${builtins.concatStringsSep " " nodePkgs}..."
-            npm install --save-dev ${builtins.concatStringsSep " " nodePkgs}
-          '';
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              nodejs_23
-              tailwindcss
-              sqlite
-              fish
-            ];
-            shellHook = ''
-              echo "Launched Transcendence DevShell"
-              echo "Using node version: `${pkgs.nodejs_23}/bin/node -v`"
-              if [ ! -d "node_modules" ]; then
-                if [ ! -f "package.json" ]; then
-                  echo "Initializing package.json..."
-                  npm init -y
-                fi
-                ${installNodePkgs}
-              else
-                echo "All node packages up-to-date!"
+          shellHook = ''
+            echo "Launched Transcendence DevShell"
+            echo "Using node version: `${pkgs.nodejs_23}/bin/node -v`"
+            if [ ! -d "node_modules" ]; then
+              if [ ! -f "package.json" ]; then
+                echo "Initializing package.json..."
+                npm init -y
               fi
-              exec fish
-            '';
-          };
-        }
-      );
-    };
+              ${installNodePkgs}
+            else
+              echo "All node packages up-to-date!"
+            fi
+            exec fish
+          '';
+        };
+      }
+    );
 }
