6595522fb97fdf575a9c0471008409ac70a7bef7
ollikarejoki <olli.karejoki@gmail.com>
2025-02-15 20:00:45 +0200

    Updated frontend, separated some functions from main.ts to new files

---
+++ frontend/src/api.ts
@@ -0,0 +1,41 @@
+
+const API_URL = "/api/auth";
+
+export async function login(username: string, password: string) {
+  try {
+    const res = await fetch(`${API_URL}/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    return await res.json();
+  } catch (err) {
+    throw new Error("Login failed!");
+  }
+}
+
+export async function register(username: string, password: string) {
+  try {
+    const res = await fetch(`${API_URL}/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    return await res.json();
+  } catch (err) {
+    throw new Error("Registration failed!");
+  }
+}
+
+export async function fetchPongData(token: string) {
+  try {
+    const res = await fetch("/api/pong", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return await res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch Pong data!");
+  }
+}
