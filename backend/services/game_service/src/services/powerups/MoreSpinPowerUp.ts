5dfd56b2a9c0dc2fa00f4d6b7e4361dc2c6e4b50
Lassi Konttinen <lassikonttinen@gmail.com>
2025-04-09 11:55:34 +0300

    Fix to negative power ups affecting the wrong player

--- backend/services/game_service/src/services/powerups/MoreSpinPowerUp.ts
+++ backend/services/game_service/src/services/powerups/MoreSpinPowerUp.ts
@@ -4,44 +4,24 @@ import PongGame from '../PongGame';
 import { PowerUp } from './PowerUp';
 
 export class MoreSpinPowerUp extends PowerUp {
+  private increase = defaultGameParams.powerUps.effects.spinIntensityIncrease;
+
   constructor(id: number, x: number, y: number) {
     super(id, x, y);
     this.type = 'more_spin';
   }
 
   applyEffect(game: PongGame, player: number): void {
-    if (player === 1) {
-      game.setSpinIntensity(
-        1,
-        game.getSpinIntensity(1) + defaultGameParams.powerUps.effects.spinIntensityIncrease
-      );
-      console.log('More spin effect applied to player 1');
-    } else {
-      game.setSpinIntensity(
-        2,
-        game.getSpinIntensity(2) + defaultGameParams.powerUps.effects.spinIntensityIncrease
-      );
-      console.log('More spin effect applied to player 2');
-    }
+    game.setSpinIntensity(player, game.getSpinIntensity(player) + this.increase);
+    console.log(`More spin effect applied to player ${player} with increase: ${this.increase}`);
     this.active = true;
     this.affectedPlayer = player;
     this.collectedTime = Date.now(); // Store the time when the power-up was collected
   }
 
   removeEffect(game: PongGame, player: number): void {
-    if (player === 1) {
-      game.setSpinIntensity(
-        1,
-        game.getSpinIntensity(1) - defaultGameParams.powerUps.effects.spinIntensityIncrease
-      );
-      console.log('More spin effect removed from player 1');
-    } else {
-      game.setSpinIntensity(
-        2,
-        game.getSpinIntensity(2) - defaultGameParams.powerUps.effects.spinIntensityIncrease
-      );
-      console.log('More spin effect removed from player 2');
-    }
+    game.setSpinIntensity(player, game.getSpinIntensity(player) - this.increase);
+    console.log(`More spin effect removed from player ${player} with decrease: ${this.increase}`);
     this.active = false;
     this.isSpent = true; // Mark the power-up as spent
   }
