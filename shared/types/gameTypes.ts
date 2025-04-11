d6cf6b4b6ce23e7e87ffd3ff83636770d08e5356
Lassi Konttinen <lassikonttinen@gmail.com>
2025-04-11 14:42:37 +0300

    Changed power up size for testing

--- shared/types/gameTypes.ts
+++ shared/types/gameTypes.ts
@@ -137,11 +137,11 @@ export const defaultGameParams: GameParams = {
     reductionFactor: 0.5, // Spin reduction on static surfaces
   },
   powerUps: {
-    minSpawnInterval: 6000, // Milliseconds
+    minSpawnInterval: 5000, // Milliseconds
     maxSpawnInterval: 10000,
     despawnTime: 10000,
     expireTime: 10000,
-    size: 50,
+    size: 80,
     effects: {
       paddleHeightIncrease: 30,
       paddleHeightDecrease: -30,
