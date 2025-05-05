c51627c636deb1676bb19a46f80986e7d0dcdf90
Lassi Konttinen <lassikonttinen@gmail.com>
2025-05-05 15:09:56 +0300

    tournament tester simulates a full tournament, including creating, joining, and playing through it

--- Makefile
+++ Makefile
@@ -54,7 +54,7 @@ fclean: down
 	@$(DOCKER_DEV) down -v --rmi all
 	@$(DOCKER_PROD) down -v --rmi all
 
-nuke: re
+nuke:
 	@echo "Deleted database"
 	@$(RM) $(DATABASE)
 
--- tests/tournament.spec.ts
+++ tests/tournament.spec.ts
@@ -1,8 +1,8 @@
-import { chromium, test, expect, Page } from '@playwright/test';
+import { chromium, expect, Page, test } from '@playwright/test';
 
 import { loginOrRegister } from '../utils/auth';
 
-const totalUsers = 7; // total number of users to simulate
+const totalUsers = 16; // total number of users to simulate
 
 test('simulate ${totalUsers} users joining tournament', async () => {
   const browser = await chromium.launch({
@@ -46,8 +46,74 @@ test('simulate ${totalUsers} users joining tournament', async () => {
     }
   }
 
+  const simulateTournamentCreator = async (i: number) => {
+    const context = await browser.newContext();
+    const { page, username } = await loginOrRegister(context, baseUrl);
+
+    await page.waitForURL(`${baseUrl}/gameMenu`);
+    console.log(`${username} navigated to game menu`);
+
+    // wait for the tournament button to be visible
+    await page
+      .getByText('Tournament', { exact: true })
+      .waitFor({ state: 'visible', timeout: 60_000 });
+    await page.getByText('Tournament').nth(0).click();
+    console.log(`${username} clicked tournament button`);
+
+    // await page.waitForURL('**/signUp');
+    // await expect(page.getByRole('heading', { name: 'Edit Profile' })).toBeVisible({
+    //   timeout: 15000,
+    // });
+    // wait for display name input to be visible
+    await page.getByLabel('Display name').waitFor({ state: 'visible', timeout: 60_000 });
+    await page.getByLabel('Display name').fill(username);
+    console.log(`Filled display name: ${username}`);
+    await page.getByRole('button', { name: 'Save' }).click();
+    await expect(page.locator('text=Edit Profile')).toHaveCount(0);
+    console.log(`Registered new user: ${username}`);
+
+    console.log(`Logged in as: ${username}`);
+
+    await page.waitForURL('**/tournament');
+    console.log(`${username} navigated to tournament page`);
+
+    // click the create tournament button
+    await page.getByRole('button', { name: 'Create' });
+    await page.getByRole('button', { name: 'Create' }).click();
+    console.log(`${username} clicked create tournament`);
+
+    // move player count slider to 16
+    const playerCountSlider = page.getByRole('slider', { name: 'Player Count' });
+    await playerCountSlider.waitFor({ state: 'visible', timeout: 60_000 });
+    await playerCountSlider.fill('16');
+    console.log(`Filled player count slider: 16`);
+
+    // input tournament name
+    const tournamentNameInput = page.getByLabel('Tournament Name:');
+    await tournamentNameInput.waitFor({ state: 'visible', timeout: 60_000 });
+    await tournamentNameInput.fill(`Tournament ${i}`);
+    console.log(`Filled tournament name: Tournament ${i}`);
+    await page.getByRole('button', { name: 'Create tournament' }).click();
+    console.log(`${username} clicked create tournament`);
+
+    // Loop through rounds until eliminated
+    let stillInTournament = true;
+    while (stillInTournament) {
+      stillInTournament = await playAndMaybeContinue(page, username);
+    }
+
+    await context.close();
+  };
+
   // Simulate a single user flow
   const simulateUser = async (i: number) => {
+    if (i === 1) {
+      // Simulate the tournament creator
+      await simulateTournamentCreator(i);
+      return;
+    }
+    // Simulate a normal user after a 15 second delay
+    await new Promise((resolve) => setTimeout(resolve, 15_000));
     const context = await browser.newContext();
     const { page, username } = await loginOrRegister(context, baseUrl);
 
@@ -85,15 +151,20 @@ test('simulate ${totalUsers} users joining tournament', async () => {
 
     // Loop through rounds until eliminated
     let stillInTournament = true;
+    let roundCounter = 1;
     while (stillInTournament) {
       stillInTournament = await playAndMaybeContinue(page, username);
+      if (stillInTournament) {
+        roundCounter++;
+        console.log(`${username} is in round ${roundCounter}`);
+      }
     }
 
     await context.close();
   };
 
-  // Simulate all users concurrently
   await Promise.all(Array.from({ length: totalUsers }, (_, i) => simulateUser(i + 1)));
+  // Simulate the tournament creator
 
   await browser.close();
 });
