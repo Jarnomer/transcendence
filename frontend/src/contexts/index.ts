1fccac2d51013d6c8c9b732f92380ef2d4c083b5
Your Name <you@example.com>
2025-05-05 21:56:57 +0400

    Added index.ts file for contexts

---
+++ frontend/src/contexts/index.ts
@@ -0,0 +1,25 @@
+// WebSocket context
+export { WebSocketProvider, useWebSocketContext } from './WebSocketContext';
+
+// User context
+export { UserProvider, useUser } from './user/UserContext';
+
+// Modals
+export { ModalProvider, useModal } from './modalContext/ModalContext';
+
+// Navigation
+export {
+  NavigationAccessProvider,
+  useNavigationAccess,
+} from './navigationAccessContext/NavigationAccessContext';
+
+// Game contexts
+export { GameOptionsProvider, useGameOptionsContext } from './gameContext/GameOptionsContext';
+export { LoadingProvider, useLoading } from './gameContext/LoadingContextProvider';
+
+// Settings contexts
+export { AudioSettingsProvider, useAudioSettings } from './audioContext/AudioSettingsContext';
+export { GraphicsSettingsProvider, useGraphicsContext } from './user/GraphicsContext';
+
+// Communication context
+export { ChatProvider, useChatContext } from './chatContext/ChatContext';
